410162869e927b5256cb7445359e5b666a8aaa57
Charlotte Moche <cmoche123@gmail.com>
2024-07-29 19:30:15 -0400

    icon

--- index.js
+++ index.js
@@ -29,30 +29,30 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 
-    function slowScrollTo(element) {
-        const targetPosition = element.offsetTop
-        const startPosition = window.scrollY
-        const distance = targetPosition - startPosition - 60
-        const duration = 1800
-        let startTime = null
+    // function slowScrollTo(element) {
+    //     const targetPosition = element.offsetTop
+    //     const startPosition = window.scrollY
+    //     const distance = targetPosition - startPosition - 60
+    //     const duration = 1800
+    //     let startTime = null
 
-        function animation(currentTime) {
-            if (startTime === null) startTime = currentTime
-            const timeElapsed = currentTime - startTime
-            const run = ease(timeElapsed, startPosition, distance, duration)
-            window.scrollTo(0, run)
-            if (timeElapsed < duration) requestAnimationFrame(animation)
-        }
+    //     function animation(currentTime) {
+    //         if (startTime === null) startTime = currentTime
+    //         const timeElapsed = currentTime - startTime
+    //         const run = ease(timeElapsed, startPosition, distance, duration)
+    //         window.scrollTo(0, run)
+    //         if (timeElapsed < duration) requestAnimationFrame(animation)
+    //     }
 
-        function ease(t, b, c, d) {
-            t /= d / 2
-            if (t < 1) return c / 2 * t * t + b
-            t--
-            return -c / 2 * (t * (t - 2) - 1) + b
-        }
+    //     function ease(t, b, c, d) {
+    //         t /= d / 2
+    //         if (t < 1) return c / 2 * t * t + b
+    //         t--
+    //         return -c / 2 * (t * (t - 2) - 1) + b
+    //     }
 
-        requestAnimationFrame(animation)
-    }
+    //     requestAnimationFrame(animation)
+    // }
 
     const body = document.body
     const toggle = document.getElementById('toggle-theme')
@@ -80,59 +80,59 @@ document.addEventListener('DOMContentLoaded', () => {
         setTheme(savedTheme)
     }
 
-    const mobileMenuOpen = document.querySelector('#mobile-menu-open')
-    const mobileMenuClose = document.querySelector('#mobile-menu-close')
-    const navItems = document.querySelector('#nav-items')
-    const navMenuOverlay = document.querySelector('#nav-menu-overlay')
-    const navLinks = document.querySelectorAll('.nav-link')
+    // const mobileMenuOpen = document.querySelector('#mobile-menu-open')
+    // const mobileMenuClose = document.querySelector('#mobile-menu-close')
+    // const navItems = document.querySelector('#nav-items')
+    // const navMenuOverlay = document.querySelector('#nav-menu-overlay')
+    // const navLinks = document.querySelectorAll('.nav-link')
 
-    const openMenu = () => {
-        navItems.classList.remove('hidden')
-        mobileMenuOpen.classList.add('hidden')
-        mobileMenuClose.classList.remove('hidden')
-        navMenuOverlay.classList.remove('hidden')
-    }
+    // const openMenu = () => {
+    //     navItems.classList.remove('hidden')
+    //     mobileMenuOpen.classList.add('hidden')
+    //     mobileMenuClose.classList.remove('hidden')
+    //     navMenuOverlay.classList.remove('hidden')
+    // }
 
-    const closeMenu = () => {
-        navItems.classList.add('hidden')
-        mobileMenuOpen.classList.remove('hidden')
-        mobileMenuClose.classList.add('hidden')
-        navMenuOverlay.classList.add('hidden')
-    }
+    // const closeMenu = () => {
+    //     navItems.classList.add('hidden')
+    //     mobileMenuOpen.classList.remove('hidden')
+    //     mobileMenuClose.classList.add('hidden')
+    //     navMenuOverlay.classList.add('hidden')
+    // }
 
-    mobileMenuOpen.addEventListener('click', openMenu)
-    mobileMenuClose.addEventListener('click', closeMenu)
-    navMenuOverlay.addEventListener('click', closeMenu)
+    // mobileMenuOpen.addEventListener('click', openMenu)
+    // mobileMenuClose.addEventListener('click', closeMenu)
+    // navMenuOverlay.addEventListener('click', closeMenu)
 
-    navLinks.forEach(link => {
-        link.addEventListener('click', (event) => {
-            event.preventDefault()
-            const targetId = event.target.getAttribute('href').replace('#', '')
-            const target = document.getElementById(targetId)
+    // navLinks.forEach(link => {
+    //     link.addEventListener('click', (event) => {
+    //         event.preventDefault()
+    //         const targetId = event.target.getAttribute('href').replace('#', '')
+    //         const target = document.getElementById(targetId)
 
-            closeMenu()
+    //         closeMenu()
 
-            slowScrollTo(target)
-        })
-    })
+    //         slowScrollTo(target)
+    //     })
+    // })
 
-    const sections = document.querySelectorAll('.section')
+    // const sections = document.querySelectorAll('.section')
 
-    sections.forEach(section => {
-        const isAboutSection = section.id === 'about'
-        const appearOnScrollOptions = {
-            threshold: isAboutSection ? 0.1 : 0.2
-        }
+    // sections.forEach(section => {
+    //     const isAboutSection = section.id === 'about'
+    //     const appearOnScrollOptions = {
+    //         threshold: isAboutSection ? 0.1 : 0.2
+    //     }
 
-        const appearOnScrollObserver = new IntersectionObserver((entries, observer) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add('appear')
-                    observer.unobserve(entry.target)
-                }
-            })
-        }, appearOnScrollOptions)
+    //     const appearOnScrollObserver = new IntersectionObserver((entries, observer) => {
+    //         entries.forEach(entry => {
+    //             if (entry.isIntersecting) {
+    //                 entry.target.classList.add('appear')
+    //                 observer.unobserve(entry.target)
+    //             }
+    //         })
+    //     }, appearOnScrollOptions)
 
-        appearOnScrollObserver.observe(section)
-    })
+    //     appearOnScrollObserver.observe(section)
+    // })
 })
