019f5285ea9ff6700d41d69379cfcf01961e852b
Charlotte Moche <cmoche123@gmail.com>
2025-05-04 13:35:49 -0400

    cursor

--- index.js
+++ index.js
@@ -37,32 +37,6 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     setupPopup('thumbnail', 'popup')
-    setupPopup('prelude-icon', 'video-popup')
-
-    // function slowScrollTo(element) {
-    //     const targetPosition = element.offsetTop
-    //     const startPosition = window.scrollY
-    //     const distance = targetPosition - startPosition - 60
-    //     const duration = 1800
-    //     let startTime = null
-
-    //     function animation(currentTime) {
-    //         if (startTime === null) startTime = currentTime
-    //         const timeElapsed = currentTime - startTime
-    //         const run = ease(timeElapsed, startPosition, distance, duration)
-    //         window.scrollTo(0, run)
-    //         if (timeElapsed < duration) requestAnimationFrame(animation)
-    //     }
-
-    //     function ease(t, b, c, d) {
-    //         t /= d / 2
-    //         if (t < 1) return c / 2 * t * t + b
-    //         t--
-    //         return -c / 2 * (t * (t - 2) - 1) + b
-    //     }
-
-    //     requestAnimationFrame(animation)
-    // }
 
     const body = document.body
     const toggle = document.getElementById('toggle-theme')
